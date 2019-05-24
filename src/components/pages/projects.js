9d57256ff739a4c6df0b5e57f09560d54a0065bd
jirawathirunkam <jirawatgot.net@gmail.com>
2019-05-24 16:39:50 -0400

    Updated Projects page again

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -9,22 +9,42 @@ class Projects extends Component {
               Software Projects
               <p></p>
               <div class="sidenav">
-                <a href="#about">Eagle Expo Demo</a>
-                <a href="#services">FGCU Real-Time Flood Sim</a>
-                <a href="#clients">FGCU VR Flood</a>
-                <a href="#contact">ADAM</a>
+                <a href="#Eagle_Expo_Demo">Eagle Expo Demo</a>
+                <a href="#FGCU_Flood_Sim">FGCU Real-Time Flood Sim</a>
+                <a href="#FGCU_VR_Flood">FGCU VR Flood Sim</a>
+                <a href="#ADAM">ADAM</a>
               </div>
             </h1>
-            <p className="EagleExpoDemo"> Eagle Expo Demo </p>
-            <p> From the video, the news discussed the other project related to hurricane,
-              but the demo presented was the Eagle Expo Demo, a virtual reality Demo that focuses on
-               interactively visualizing the architecture of a construction project.
+            <a name="Eagle_Expo_Demo"></a>
+            <p className="ProjectTitles"> Eagle Expo Demo</p>
+            <p> I was on WINK News talking about virtual reality and the VIPER Lab where I worked at and
+            the demo presented was the Eagle Expo Demo, a virtual reality Demo that focuses on
+            interactively visualizing the architecture of a construction project.
+            The purpos of this project is to help engineers be able to observe 3D data in an immersive environment which helps in detecting defects and also a good way of presenting
+            the construction project to a client before it is built.
             </p>
             <br/>
             <iframe className="EagleExpoDemoVideo"
              src="https://drive.google.com/file/d/1efeRGHd8pxeq61jJ9IYLmRkhGfrpJdFk/preview">
             </iframe>
-            <p className="EagleExpoDemoEndBorder"></p>
+            <p className="border"></p>
+            <a name="FGCU_Flood_Sim"></a>
+            <p className="ProjectTitles"> FGCU Real-Time Flood Simulation</p>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+            </p>
+            <p className="border"></p>
+            <a name="FGCU_VR_Flood"></a>
+            <p className="ProjectTitles"> FGCU VR Flood Simulation</p>
+            <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+            </p>
+            <p className="border"></p>
+            <a name="ADAM"></a>
+            <p className="ProjectTitles"> Automatic Directional Antenna Mechanism (ADAM)</p>
+            <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+            </p>
+
+            <br/>
+            <br/>
           </body>
 
       </div>
